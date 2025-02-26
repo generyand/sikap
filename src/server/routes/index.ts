cf531b52294bef564dd80529433732179ae94823
generyand <generyan.dep@gmail.com>
2025-02-26 09:42:13 +0800

    removed unneeded codes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import { authRoutes } from './auth.routes';
 import { taskRoutes } from './task.routes';
 import { profileRoutes } from './profile.routes';
 
 const router = Router();
 
-router.use('/auth', authRoutes);
 router.use('/tasks', taskRoutes);
 router.use('/profiles', profileRoutes);
 
